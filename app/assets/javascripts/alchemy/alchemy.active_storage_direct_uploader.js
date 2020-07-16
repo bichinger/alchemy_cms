7812c6c4f2b493d92961cd945a0ecbad13c32a66
Swen-Thorsten Mock <s.mock@bichinger.de>
2020-07-16 17:21:21 +0200

    Working on ActiveStorageFile archive (FE/Templates).

--- app/assets/javascripts/alchemy/alchemy.active_storage_direct_uploader.js
+++ app/assets/javascripts/alchemy/alchemy.active_storage_direct_uploader.js
@@ -2,13 +2,13 @@ Alchemy.ActiveStorageDirectUploader = function (settings) {
 
   settings.fileInput.addEventListener('change', (event) => {
     Array.from(settings.fileInput.files).forEach(file => uploadFile(file))
-    // you might clear the selected files from the input
+    // clear the selected files from the input
     settings.fileInput.value = null
   })
 
   const uploadFile = (file) => {
     // your form needs the file_field direct_upload: true, which
-    //  provides data-direct-upload-url
+    // provides data-direct-upload-url
     const url = settings.fileInput.dataset.directUploadUrl
     const upload = new ActiveStorage.DirectUpload(file, url)
 
@@ -16,23 +16,43 @@ Alchemy.ActiveStorageDirectUploader = function (settings) {
       console.log('create: ', error, blob);
       if (error) {
 
-        // Handle the error
+        // TODO: Handle the error
         console.error('File upload failed: ', error);
 
       } else {
-        console.log('adding hidden field . . . ', settings);
+        console.log('adding hidden field . . . ', settings, blob);
 
         let $fileUploadForm = $(settings.fileUploadForm);
 
-        $.post(
+        let request = $.post(
           $fileUploadForm.attr('action'),
           {
             authenticity_token: $fileUploadForm.find('input[name=authenticity_token]').val(),
             active_storage_file: {
               file: blob.signed_id,
+              name: blob.filename,
             }
           }
-        );
+        )
+          .done(function () {
+            console.log('upload successfully done . . . redirecting');
+
+            if (settings.in_dialog) {
+              $.get(settings.redirect_url, null, null, 'script');
+            }
+            else {
+              Turbolinks.visit(settings.redirect_url);
+            }
+
+          })
+          .fail(function () {
+
+            // TODO: Handle the error
+            console.error('File upload failed: ', error);
+
+          })
+          // .always(function () {
+          // });
       }
     })
 
